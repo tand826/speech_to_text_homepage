a8960241a212452f7360d38344ef90ea5f04d9cf
tand826 <takumi.ando826@gmail.com>
2022-02-11 09:32:08 +0900

    vite settings updated for deployment page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base: "https://tand826.github.io/speech_to_text_homepage",
+  plugins: [vue()],
+  build: {
+    outDir: "docs",
+  },
+});
