bb2887e3183ffbc85239d810e1c6824184d3295a
tand826 <takumi.ando826@gmail.com>
2022-02-11 11:24:45 +0900

    update

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -13,118 +13,99 @@
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols3 gap-4 md:gap-6 xl:gap-8">
         <!-- image - start -->
-        <a
-          href="#"
+
+        <span
           class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
         >
-          <img
-            src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
-            loading="lazy"
-            alt="Photo by Minh Pham"
+          <iframe
             class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-          />
-
-          <div
-            class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"
-          ></div>
-
+            src="https://www.youtube.com/embed/Mw7w0uPUlu4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
           <span
-            class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
+            class="inline-block text-gray-900 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
             >Installation</span
           >
-        </a>
+        </span>
         <!-- image - end -->
 
         <!-- image - start -->
-        <a
-          href="#"
+        <span
           class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
         >
-          <img
-            src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
-            loading="lazy"
-            alt="Photo by Lorenzo Herrera"
+          <iframe
             class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-          />
-
-          <div
-            class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"
-          ></div>
-
+            src="https://www.youtube.com/embed/tPcH9yNuko8"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
           <span
-            class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
+            class="inline-block text-gray-900 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
             >Recognize your voice (single phrase)</span
           >
-        </a>
+        </span>
         <!-- image - end -->
 
         <!-- image - start -->
-        <a
-          href="#"
+        <span
           class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
         >
-          <img
-            src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
-            loading="lazy"
-            alt="Photo by Lorenzo Herrera"
+          <iframe
             class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-          />
-
-          <div
-            class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"
-          ></div>
-
+            src="https://www.youtube.com/embed/Xx9WdwC5gsE"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
           <span
-            class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
+            class="inline-block text-gray-900 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
             >Recognize your voice (multiple phrase)</span
           >
-        </a>
+        </span>
         <!-- image - end -->
 
         <!-- image - start -->
-        <a
-          href="#"
+        <span
           class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
         >
-          <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=600"
-            loading="lazy"
-            alt="Photo by Magicle"
+          <iframe
             class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-          />
-
-          <div
-            class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"
-          ></div>
-
+            src="https://www.youtube.com/embed/kmSNdiuMX9g"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
           <span
-            class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
+            class="inline-block text-gray-900 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
             >Copy history to clipboard (single history)</span
           >
-        </a>
+        </span>
         <!-- image - end -->
 
         <!-- image - start -->
-        <a
-          href="#"
+        <span
           class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
         >
-          <img
-            src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=600"
-            loading="lazy"
-            alt="Photo by Magicle"
+          <iframe
             class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-          />
-
-          <div
-            class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"
-          ></div>
-
+            src="https://www.youtube.com/embed/XFeWb5FSjJk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
           <span
-            class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
-            >Copy history to clipboard (multiple history)</span
+            class="inline-block text-gray-000 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3"
+            >Copy history to clipboard (multiple histories)</span
           >
-        </a>
+        </span>
         <!-- image - end -->
       </div>
     </div>
